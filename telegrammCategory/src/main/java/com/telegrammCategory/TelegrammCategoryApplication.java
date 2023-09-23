9102f29d6f44265fbed015a6e9482d293de0a7ce
igr76 <gritsuk766@gmail.com>
2023-09-23 13:14:44 +1000

    start

---
+++ telegrammCategory/src/main/java/com/telegrammCategory/TelegrammCategoryApplication.java
@@ -0,0 +1,13 @@
+package com.telegrammCategory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrammCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegrammCategoryApplication.class, args);
+	}
+
+}
---
+++ telegrammCategory/src/test/java/com/telegrammCategory/TelegrammCategoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegrammCategory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegrammCategoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
