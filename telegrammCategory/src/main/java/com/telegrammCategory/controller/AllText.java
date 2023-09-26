afa6672d2b50b55fed1040b97a134e72527bdc50
igr76 <gritsuk766@gmail.com>
2023-09-27 00:01:19 +1000

    написание логики, сущности, репозитория

---
+++ telegrammCategory/src/main/java/com/telegrammCategory/controller/AllText.java
@@ -0,0 +1,14 @@
+package com.telegrammCategory.controller;
+
+public class AllText {
+    public static final String DDC_MESSAGE_UPDATE = "doc_message_update";
+    public static final String PHOTO_MESSAGE_UPDATE = "photo_message_update";
+    public static final String START = "/start";
+    public static final String GET = "/get";
+    public static final String GREAT = "/great";
+    public static final String GREAT_NEW = "/greatNew";
+    public static final String DELETE = "/delete";
+    public static final String HELP = "/help";
+    static final String NEXT = "/next";
+    static final String PREVIOUS = "/previous";
+}
---
+++ telegrammCategory/src/main/java/com/telegrammCategory/service/impl/UpdateProducerImpl.java
@@ -0,0 +1,17 @@
+package com.telegrammCategory.service.impl;
+
+import com.telegrammCategory.service.UpdateProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UpdateProducerImpl implements UpdateProducer {
+    @Override
+    public void produce(String rabbitQueue, Update update) {
+        log.debug(update.getMessage().getText());
+    }
+}
